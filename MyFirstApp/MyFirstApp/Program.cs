dcce87922cc6d96483efc3bb5a571bcbecadfe19
KrAgaciakStudenc <s28585@pjwstk.edu.pl>
2024-03-12 15:13:38 +0100

    Add GertAvarage methode

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -3,3 +3,14 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+static double GetAvarage(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.Length;
+}
